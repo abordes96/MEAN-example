e2baab189308e659f764e77fdbb85b1cddae9023
Antonio Bordes Gimenez <abordesgimenez@gmail.com>
2019-03-04 00:20:45 +0100

    DB functions implemented

--- server/controllers/employee.controller.js
+++ server/controllers/employee.controller.js
@@ -1,27 +1,45 @@
-const employee = require('../models/employee');
+const Employee = require('../models/employee');
 
 const employeeCtrl = {};
 
-employeeCtrl.getEmployees = (req, res) => {
-    employee.find();
-}
+employeeCtrl.getEmployees = async(req, res) => {
+     const employees = await Employee.find();
+     res.json(employees);
+};
 
-employeeCtrl.getEmployee = (req, res) => {
+employeeCtrl.getEmployee = async(req, res) => {
+    const employee = await Employee.findById(req.params.id);
+    res.json(employee);
+};
+
+employeeCtrl.createEmployee = async(req, res) => {
+    const employee = new Employee(req.body);
+    await employee.save();
     res.json({
-        status: 'Employee goes here'
+        "status":"Employee saved"
+    });
+};
+
+employeeCtrl.editEmployee = async(req, res) => {
+    const {id} = req.params;
+    const employee = {
+        "name":req.body.name,
+        "position":req.body.position,
+        "office":req.body.office,
+        "salary":req.body.salary
+    };
+    //Con new: true si el empleado no existe lo crea
+    await Employee.findByIdAndUpdate(id,{$set:employee},{new: true});
+    res.json({
+        "status":"Employee updated"
     });
 }
 
-employeeCtrl.createEmployee = function(){
-
-}
-
-employeeCtrl.editEmployee = function(){
-
-}
-
-employeeCtrl.deleteEmployee = function(){
-
+employeeCtrl.deleteEmployee =  async (req,res) => {
+    await Employee.findByIdAndDelete(req.params.id);
+    res.json({
+        "status":"Employee deleted"
+    });
 }
 
 module.exports = employeeCtrl;
